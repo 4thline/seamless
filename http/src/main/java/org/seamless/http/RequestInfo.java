29487e71492f9efb83a2e0f685deb0dc2febb193
Christian Bauer <cb@4thline.com>
2012-12-17 22:04:54 +0100

    Added various utilities to RequestInfo

--- http/src/main/java/org/seamless/http/RequestInfo.java
+++ http/src/main/java/org/seamless/http/RequestInfo.java
@@ -17,11 +17,15 @@ package org.seamless.http;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Enumeration;
+import java.util.logging.Logger;
 
 /**
  * @author Christian Bauer
+ * @author Michael Pujos
  */
 public class RequestInfo {
+
+    final private static Logger log = Logger.getLogger(RequestInfo.class.getName());
 
     public static void reportRequest(StringBuilder builder, HttpServletRequest req) {
         builder.append("Request: ");
@@ -89,7 +93,7 @@ public class RequestInfo {
 
     public static void reportCookies(StringBuilder builder, HttpServletRequest req) {
         Cookie[] cookies = req.getCookies();
-        if (cookies == null)  return;
+        if (cookies == null) return;
         int l = cookies.length;
         if (l > 0) {
             builder.append("Cookies:\n");
@@ -107,6 +111,104 @@ public class RequestInfo {
         builder.append("Remote Port: ").append(req.getRemotePort()).append("\n");
         if (req.getRemoteUser() != null)
             builder.append("Remote User: ").append(req.getRemoteUser()).append("\n");
+    }
+
+    public static boolean isPS3Request(String userAgent, String avClientInfo) {
+        return ((userAgent != null && userAgent.contains("PLAYSTATION 3")) ||
+            (avClientInfo != null && avClientInfo.contains("PLAYSTATION 3")));
+    }
+
+    public static boolean isAndroidBubbleUPnPRequest(String userAgent) {
+        return (userAgent != null && userAgent.contains("BubbleUPnP"));
+    }
+
+    public static boolean isPS3Request(HttpServletRequest request) {
+        return isPS3Request(request.getHeader("User-Agent"), request.getHeader("X-AV-Client-Info"));
+    }
+
+    public static boolean isJRiverRequest(HttpServletRequest request) {
+        return isJRiverRequest(request.getHeader("User-Agent"));
+    }
+
+    public static boolean isJRiverRequest(String userAgent) {
+        return userAgent != null && (userAgent.contains("J-River") || userAgent.contains("J. River"));
+    }
+
+    public static boolean isWMPRequest(String userAgent) {
+        return userAgent != null && userAgent.contains("Windows-Media-Player") && !isJRiverRequest(userAgent);
+    }
+
+    public static boolean isXbox360Request(HttpServletRequest request) {
+        return isXbox360Request(request.getHeader("User-Agent"), request.getHeader("Server"));
+    }
+
+    public static boolean isXbox360Request(String userAgent, String server) {
+        return (userAgent != null && (userAgent.contains("Xbox") || userAgent.contains("Xenon"))) ||
+            (server != null && server.contains("Xbox"));
+    }
+
+    public static boolean isXbox360AlbumArtRequest(HttpServletRequest request) {
+        return "true".equals(request.getParameter("albumArt")) && isXbox360Request(request);
+    }
+
+    public static void dumpRequestHeaders(long timestamp, HttpServletRequest request) {
+        dumpRequestHeaders(timestamp, "REQUEST HEADERS", request);
+    }
+
+    public static void dumpRequestString(long timestamp, HttpServletRequest request) {
+        log.info(getRequestInfoString(timestamp, request));
+    }
+
+    public static void dumpRequestHeaders(long timestamp, String text, HttpServletRequest request) {
+        log.info(text);
+        dumpRequestString(timestamp, request);
+        Enumeration headers = request.getHeaderNames();
+        if (headers != null) {
+            while (headers.hasMoreElements()) {
+                String headerName = (String) headers.nextElement();
+                log.info(String.format("%s: %s", headerName, request.getHeader(headerName)));
+            }
+        }
+        log.info("----------------------------------------");
+    }
+
+    public static String getRequestInfoString(long timestamp, HttpServletRequest request) {
+        return String.format("%s %s %s %s %s %d",
+            request.getMethod(),
+            request.getRequestURI(),
+            request.getProtocol(),
+            request.getParameterMap(),
+            request.getRemoteAddr(),
+            timestamp);
+    }
+
+    public static String getRequestFullURL(HttpServletRequest req) {
+
+        String scheme = req.getScheme();             // http
+        String serverName = req.getServerName();     // hostname.com
+        int serverPort = req.getServerPort();        // 80
+        String contextPath = req.getContextPath();   // /mywebapp
+        String servletPath = req.getServletPath();   // /servlet/MyServlet
+        String pathInfo = req.getPathInfo();         // /a/b;c=123
+        String queryString = req.getQueryString();   // d=789
+
+        // Reconstruct original requesting URL
+        StringBuffer url = new StringBuffer();
+        url.append(scheme).append("://").append(serverName);
+
+        if ((serverPort != 80) && (serverPort != 443)) {
+            url.append(":").append(serverPort);
+        }
+
+        url.append(contextPath).append(servletPath);
+
+        if (pathInfo != null) {
+            url.append(pathInfo);
+        }
+        if (queryString != null) {
+            url.append("?").append(queryString);
+        }
+        return url.toString();
     }
 
 }
